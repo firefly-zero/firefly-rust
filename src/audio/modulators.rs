09ffa64c57190bf9d6ebafff949a91858c86c6a0
gram <git@orsinium.dev>
2024-09-11 20:29:54 +0200

    add more sources and modulators

--- src/audio/modulators.rs
+++ src/audio/modulators.rs
@@ -26,10 +26,38 @@ impl Modulator for LinearModulator {
     }
 }
 
+pub struct Hold {
+    pub before: f32,
+    pub after: f32,
+    pub time: Time,
+}
+
+impl Modulator for Hold {
+    fn modulate(self, node_id: u32, param: u32) {
+        unsafe {
+            bindings::mod_hold(node_id, param, self.before, self.after, self.time.0);
+        }
+    }
+}
+
+pub struct Sine {
+    pub freq: Freq,
+    pub low: f32,
+    pub high: f32,
+}
+
+impl Modulator for Sine {
+    fn modulate(self, node_id: u32, param: u32) {
+        unsafe {
+            bindings::mod_sine(node_id, param, self.freq.0, self.low, self.high);
+        }
+    }
+}
+
 mod bindings {
     #[link(wasm_import_module = "audio")]
     extern {
-        pub(crate) fn mod_linear(
+        pub(super) fn mod_linear(
             node_id: u32,
             param: u32,
             start: f32,
@@ -37,5 +65,7 @@ mod bindings {
             start_at: u32,
             end_at: u32,
         );
+        pub(super) fn mod_hold(node_id: u32, param: u32, v1: f32, v2: f32, time: u32);
+        pub(super) fn mod_sine(node_id: u32, param: u32, freq: f32, low: f32, high: f32);
     }
 }
--- src/audio/nodes.rs
+++ src/audio/nodes.rs
@@ -5,6 +5,10 @@ use super::*;
 pub struct Root {}
 pub struct Sine {}
 pub struct Gain {}
+pub struct Square {}
+pub struct Noise {}
+pub struct Empty {}
+pub struct Zero {}
 
 pub struct Node<F> {
     id: u32,
@@ -24,6 +28,26 @@ impl<F> Node<F> {
 
     pub fn add_sine(&self, f: Freq, phase: f32) -> Node<Sine> {
         let id = unsafe { bindings::add_sine(self.id, f.0, phase) };
+        Node::new(id)
+    }
+
+    pub fn add_square(&self, f: Freq, phase: f32) -> Node<Square> {
+        let id = unsafe { bindings::add_square(self.id, f.0, phase) };
+        Node::new(id)
+    }
+
+    pub fn add_noise(&self, seed: i32) -> Node<Noise> {
+        let id = unsafe { bindings::add_noise(self.id, seed) };
+        Node::new(id)
+    }
+
+    pub fn add_empty(&self) -> Node<Empty> {
+        let id = unsafe { bindings::add_empty(self.id) };
+        Node::new(id)
+    }
+
+    pub fn add_zero(&self) -> Node<Zero> {
+        let id = unsafe { bindings::add_zero(self.id) };
         Node::new(id)
     }
 
@@ -54,11 +78,14 @@ impl Node<Gain> {
 mod bindings {
     #[link(wasm_import_module = "audio")]
     extern {
-        pub(crate) fn add_sine(parent_id: u32, freq: f32, phase: f32) -> u32;
-        // pub(crate) fn add_square(parent_id: u32, freq: f32, phase: f32) -> u32;
-        pub(crate) fn add_gain(parent_id: u32, lvl: f32) -> u32;
-        pub(crate) fn reset(node_id: u32);
-        pub(crate) fn reset_all(node_id: u32);
-        pub(crate) fn clear(node_id: u32);
+        pub(super) fn add_sine(parent_id: u32, freq: f32, phase: f32) -> u32;
+        pub(super) fn add_gain(parent_id: u32, lvl: f32) -> u32;
+        pub(super) fn add_square(parent_id: u32, freq: f32, phase: f32) -> u32;
+        pub(super) fn add_noise(parent_id: u32, seed: i32) -> u32;
+        pub(super) fn add_empty(parent_id: u32) -> u32;
+        pub(super) fn add_zero(parent_id: u32) -> u32;
+        pub(super) fn reset(node_id: u32);
+        pub(super) fn reset_all(node_id: u32);
+        pub(super) fn clear(node_id: u32);
     }
 }
