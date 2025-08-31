1d9996b72eb0fbad406a6f6acb4507bad8924fc7
gram <git@orsinium.dev>
2025-08-31 18:22:28 +0200

    add get_name

--- src/misc.rs
+++ src/misc.rs
@@ -1,3 +1,5 @@
+use crate::net::Peer;
+
 /// Log a debug message.
 pub fn log_debug(t: &str) {
     let ptr = t.as_ptr() as u32;
@@ -29,6 +31,23 @@ pub fn get_random() -> u32 {
     unsafe { bindings::get_random() }
 }
 
+/// Get the Peer's name.
+#[cfg(feature = "alloc")]
+#[must_use]
+pub fn get_name_buf(p: Peer) -> alloc::string::String {
+    let mut buf = [0u8; 16];
+    let name = get_name(p, &mut buf);
+    name.to_owned()
+}
+
+#[must_use]
+pub fn get_name(p: Peer, buf: &mut [u8; 16]) -> &str {
+    let ptr = buf.as_ptr() as u32;
+    let len = unsafe { bindings::get_name(u32::from(p.0), ptr) };
+    let buf = &buf[..len as usize];
+    unsafe { core::str::from_utf8_unchecked(buf) }
+}
+
 /// Exit the app after the current update is finished.
 pub fn quit() {
     unsafe { bindings::quit() }
@@ -41,6 +60,7 @@ mod bindings {
         pub(crate) fn log_error(ptr: u32, len: u32);
         pub(crate) fn set_seed(seed: u32);
         pub(crate) fn get_random() -> u32;
+        pub(crate) fn get_name(idx: u32, ptr: u32) -> u32;
         pub(crate) fn quit();
     }
 }
