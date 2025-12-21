20086b2dbd4ea6b73411e8aa72c0dff3b2c99b79
gram <git@orsinium.dev>
2025-12-21 18:59:07 +0100

    add font helper methods

--- src/graphics/font.rs
+++ src/graphics/font.rs
@@ -2,10 +2,38 @@ use crate::*;
 
 /// A loaded font file.
 ///
-/// Can be loaded as [`FileBuf`] from ROM with [`rom::load_buf`]
-/// and then cast using [Into].
+/// Can be loaded as [`FileBuf`] from ROM with [`load_file_buf`]
+/// and then cast using [`FileBuf::as_font`].
 pub struct Font<'a> {
     pub(crate) raw: &'a [u8],
+}
+
+impl Font<'_> {
+    /// Check if the font encoding is ASCII.
+    #[must_use]
+    pub fn is_ascii(&self) -> bool {
+        self.raw[1] == 0
+    }
+
+    /// Calculate width (in pixels) of the given text.
+    ///
+    /// This function does not account for newlines.
+    #[must_use]
+    pub fn line_width(&self, t: &str) -> u32 {
+        t.len() as u32 * u32::from(self.char_width())
+    }
+
+    /// The width (in pixels) of one character.
+    #[must_use]
+    pub fn char_width(&self) -> u8 {
+        self.raw[2]
+    }
+
+    /// The hight (in pixels) of one character (one line).
+    #[must_use]
+    pub fn char_height(&self) -> u8 {
+        self.raw[3]
+    }
 }
 
 impl<'a> From<File<'a>> for Font<'a> {
